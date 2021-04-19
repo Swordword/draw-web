f30a12858b7e25858c83fa49292072399f5edf6f
John Massive <massivejohn123@gmail.com>
2021-04-19 11:20:20 +0800

    fp

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// NATIVE
+const path = require('path')
+// PACKAGE
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: "draw site"
+    }),
+  ],
+}
